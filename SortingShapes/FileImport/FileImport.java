4d19763820abe3dda87a9067ee448cdb862e0fd9
Daniel <51567745+daniel-barbieri00@users.noreply.github.com>
2023-10-19 10:41:16 -0600

    Shape Type Addition
    Adding functionality so it will add the categorized shape to a list of Shapes built off the shapeADT

--- SortingShapes/FileImport/FileImport.java
+++ SortingShapes/FileImport/FileImport.java
@@ -1,15 +1,17 @@
-package FileImport;
+package SortingShapes.FileImport;
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import shapes.shapes;
+
+import SortingShapes.shapes.*;
 
 
 public class FileImport {
 	List<List> full = new ArrayList<List>();
-	List<shapes> Shapes = new ArrayList<shapes>();
+	List<shapeADT> Shapes = new ArrayList<>();
 	
 	/**
 	 * Reads from the given file and separates all contents into classes
@@ -33,10 +35,35 @@ public class FileImport {
 					String names = values[i];
 					String v1 = values[i+1];
 					String v2 = values[i+2];
-					shapes Shape = new shapes(names, v1, v2);
 					
-					Shapes.add(Shape);
-					
+					if (names == "Cone") {
+						shapeADT Shape = new cone(names, Double.parseDouble(v1), Double.parseDouble(v2));
+						Shapes.add(Shape);
+					}
+					else if (names == "Cylinder") {
+						shapeADT Shape = new cylinder(names, Double.parseDouble(v1), Double.parseDouble(v2));
+						Shapes.add(Shape);
+					}
+					else if (names == "OctagonalPrism") {
+						shapeADT Shape = new octagonBase(names, Double.parseDouble(v1), Double.parseDouble(v2));
+						Shapes.add(Shape);
+					}
+					else if (names == "PentagonalPrism") {
+						shapeADT Shape = new pentagonBase(names, Double.parseDouble(v1), Double.parseDouble(v2));
+						Shapes.add(Shape);
+					}
+					else if (names == "Pyramid") {
+						shapeADT Shape = new pyramid(names, Double.parseDouble(v1), Double.parseDouble(v2));
+						Shapes.add(Shape);
+					}
+					else if (names == "SquarePrism") {
+						shapeADT Shape = new retangularPrism(names, Double.parseDouble(v1), Double.parseDouble(v2));
+						Shapes.add(Shape);
+					}
+					else if (names == "TriangularPrism") {
+						shapeADT Shape = new triangleBase(names, Double.parseDouble(v1), Double.parseDouble(v2));
+						Shapes.add(Shape);
+					}
 ;					
 				}
 			}
