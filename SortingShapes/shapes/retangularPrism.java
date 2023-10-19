5c346ec0d6b35e4c3d6627cfe9d830633e297a24
Daniel <51567745+daniel-barbieri00@users.noreply.github.com>
2023-10-19 10:49:15 -0600

    Adding Attribute Type
    Adding attribute type for list adding purposes

--- SortingShapes/shapes/retangularPrism.java
+++ SortingShapes/shapes/retangularPrism.java
@@ -1,17 +1,25 @@
-public class triangleBase extends shapeADT {
+package SortingShapes.shapes;
+import java.util.*;
+import java.math.*;
+
+public class retangularPrism extends shapeADT {
+	String type;
     double height;
     double length;
 
-    public triangleBase(double length, double height) {
-        this.height = height;
+    public retangularPrism(String type, double length, double height) {
+        this.type = type;
+    	this.height = height;
         this.length = length;
     }
 
     public double calculateArea(double l, double h) {
-        double baseArea = l * 2;
+        double area = l * 2;
+        return area;
     }
 
     public double calculateVolume(double l, double h) {
         double volume = l * 2 * h;
+        return volume;
     }
 }
