b26b8dc25d2ece2bab5f635b7774e06fb574a23c
KeeganM22 <119663094+KeeganM22@users.noreply.github.com>
2023-10-19 15:13:26 -0600

    Update FileImport.java
    final version of file importing. make changes to fit with classes. classes should take the name of the super class of shape and that is how the algorithm should realize math it needs

--- SortingShapes/FileImport/FileImport.java
+++ SortingShapes/FileImport/FileImport.java
@@ -1,67 +1,51 @@
-package SortingShapes.FileImport;
+package FileImport;
 import java.io.File;
-
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import SortingShapes.shapes.*;
+
+import shapes.shapes;
+
 
 
 public class FileImport {
-	List<List> full = new ArrayList<List>();
-	List<shapeADT> Shapes = new ArrayList<>();
-	
 	/**
 	 * Reads from the given file and separates all contents into classes
+	 * 
 	 * @param f = filename, the name of the file
 	 */
+	
+	// change the "shapes" part to whatever is the super class of all shapes
+	public List<shapes> Shapes = new ArrayList<shapes>();
+	
 	public FileImport() {
 		
 	}
 	
 	public void readFile(String f) {
+		File file = new File(f);
+		
+		Scanner scan;
 		try {
-			// This part creates a file called fil then creates a scanner reading fil
-			File fil = new File(f);
-			Scanner myReader = new Scanner(fil);
-			// this is the loop that goes through the file
-			while (myReader.hasNextLine()) {
-				String data = myReader.nextLine();
-				String[] values = data.split(" ");
-				int i;
-				for (i = 0; i < values.length; i+=3) {
-					String names = values[i];
-					String v1 = values[i+1];
-					String v2 = values[i+2];
-										
-					switch (names) {
-					case "Cone":
-						//Cone cone = new cone(names, v1, v2)
-					case "SquarePrism":
-						//Sqp sqp = new Sqp(names, v1, v2)
-					case "OctagonalPrism":
-						//Ocp ocp = new Ocp(names, v1, v2)
-					case "PentagonalPrism":
-						//Pep pep = new Pep(names, v1, v2)
-					case "Pyramid":
-						//Pyrimid pyramid = new Pyramid(names, v1, v2)
-					case "TriangularPrism":
-						//Trp trp = new Trp(names, v1, v2)
-					}
-;					
-				}
+			scan = new Scanner(file);
+			scan.useDelimiter(" ");
+			int amount = Integer.parseInt(scan.next());
+			while (scan.hasNext()) {
+				String name = scan.next();
+				Double v1 = Double.parseDouble(scan.next());
+				Double v2 = Double.parseDouble(scan.next());
+				shapes shape = new shapes(name, v1, v2);
+				Shapes.add(shape);
 			}
 			System.out.println(Shapes.size());
-			
-			myReader.close();
+		
 		} catch (FileNotFoundException e) {
-			System.out.println("An error occurred.");
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 	}
 	
-
 }
