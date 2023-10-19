3801ff6c964c1c9660925a0bbdc6b02fac14d6ca
Daniel <51567745+daniel-barbieri00@users.noreply.github.com>
2023-10-19 10:47:37 -0600

    Adding Attribute Type
    Adding attribute type for list adding purposes

--- SortingShapes/shapes/pentagonBase.java
+++ SortingShapes/shapes/pentagonBase.java
@@ -1,19 +1,25 @@
+package SortingShapes.shapes;
+import java.util.*;
+import java.math.*;
+
 public class pentagonBase extends shapeADT {
+	String type;
     double height;
     double length;
 
-    public pentagonBase(double length, double height) {
-        this.height = height;
+    public pentagonBase(String type, double length, double height) {
+        this.type = type;
+    	this.height = height;
         this.length = length;
     }
 
     public double calculateArea(double l, double h) {
-        double area = 5 * l * 2 * tan(54) * 4;
+        double area = 5 * l * 2 * Math.tan(54) * 4;
         return area;
     }
 
     public double calculateVolume(double l, double h) {
-        double volume = (5 * l * 2 * tan(54) * 4) * h;
+        double volume = (5 * l * 2 * Math.tan(54) * 4) * h;
         return volume;
     }
 }
