a479d111d2f9e5ddb8e39e23741ee2575d2bcdbd
KeeganM22 <119663094+KeeganM22@users.noreply.github.com>
2023-10-20 18:42:30 -0600

    Update Tests.java
    forgot to save before commiting, now added

--- SortingShapes/Tests.java
+++ SortingShapes/Tests.java
@@ -1,6 +1,10 @@
 package testenv;
+import shapes;
+import FileImport.FileImport;
 
 import static org.junit.Assert.*;
+
+import java.util.Scanner;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -14,14 +18,101 @@ public class Tests {
 	@Before
 	public void setUp() throws Exception {
 		
+		// util should be able to call all sorts and the return the list afterwards
+		Utility util = new Utility();
 		
 	}
 
 	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    public void testBubbleSort() {
+		startTime = System.currentTimeMillis();
+        List<shapeADT> list = new arrayList<shapeADT> = util.bubbleSort();
+        list.get(0);
+        int i;
+        for (i =0; i< list.size(); i+=1000) {
+        	list.get(i);
+        }
+        list.get(list.size() - 1);
+        endTime = System.currentTimeMillis();
+        System.out.println("Quick Sort Time: " + (endTime - startTime) + " ms");
+
+    }
+
+    @Test
+    public void testInsertionSort() {
+    	startTime = System.currentTimeMillis();
+    	List<shapeADT> list = new arrayList<shapeADT> = util.bubbleSort();
+        list.get(0);
+        int i;
+        for (i =0; i< list.size(); i+=1000) {
+        	list.get(i);
+        }
+        list.get(list.size() - 1);
+    	endTime = System.currentTimeMillis();
+        System.out.println("Quick Sort Time: " + (endTime - startTime) + " ms");
+
+    }
+
+    @Test
+    public void testGnomeSort() {
+    	startTime = System.currentTimeMillis();
+    	List<shapeADT> list = new arrayList<shapeADT> = util.bubbleSort();
+        list.get(0);
+        int i;
+        for (i =0; i< list.size(); i+=1000) {
+        	list.get(i);
+        }
+        list.get(list.size() - 1);
+    	endTime = System.currentTimeMillis();
+        System.out.println("Quick Sort Time: " + (endTime - startTime) + " ms");
+
+    }
+
+    @Test
+    public void testMergeSort() {
+    	startTime = System.currentTimeMillis();
+    	List<shapeADT> list = new arrayList<shapeADT> = util.bubbleSort();
+        list.get(0);
+        int i;
+        for (i =0; i< list.size(); i+=1000) {
+        	list.get(i);
+        }
+        list.get(list.size() - 1);
+    	endTime = System.currentTimeMillis();
+        System.out.println("Quick Sort Time: " + (endTime - startTime) + " ms");
+
+    }
+
+    @Test
+    public void testQuickSort() {
+    	startTime = System.currentTimeMillis();
+    	List<shapeADT> list = new arrayList<shapeADT> = util.bubbleSort();
+        list.get(0);
+        int i;
+        for (i =0; i< list.size(); i+=1000) {
+        	list.get(i);
+        }
+        list.get(list.size() - 1);
+    	endTime = System.currentTimeMillis();
+        System.out.println("Quick Sort Time: " + (endTime - startTime) + " ms");
+
+    }
+
+    @Test
+    public void testSelectionSort() {
+    	startTime = System.currentTimeMillis();
+    	List<shapeADT> list = new arrayList<shapeADT> = util.bubbleSort();
+        list.get(0);
+        int i;
+        for (i =0; i< list.size(); i+=1000) {
+        	list.get(i);
+        }
+        list.get(list.size() - 1);
+    	endTime = System.currentTimeMillis();
+        System.out.println("Quick Sort Time: " + (endTime - startTime) + " ms");
+
+    }
 
 }
