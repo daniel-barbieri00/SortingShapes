d2f87a0f85ce8d8047636b725482bc96c472ccc9
KeeganM22 <119663094+KeeganM22@users.noreply.github.com>
2023-10-20 18:28:24 -0600

    added tests for sorting algorithms
    make sure that Junit 4 is in here

---
+++ SortingShapes/Tests.java
@@ -0,0 +1,27 @@
+package testenv;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Tests {
+
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		
+	}
+
+	
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
